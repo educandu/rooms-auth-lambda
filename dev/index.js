1e1fc9e5f3f9d269ece70a36bd7e6a6fd952361d
Andreas Helmberger <public@ahelmberger.de>
2022-01-04 16:24:31 +0100

    EDU-197 Add dev-server

--- dev/index.js
+++ dev/index.js
@@ -2,4 +2,5 @@ export { jest } from './jest.js';
 export { cliArgs } from './cli.js';
 export { Process } from './process.js';
 export { NodeProcess } from './node-process.js';
-export { delay, glob, isMac, kebabToCamel, noop } from './helpers.js';
+export { delay, glob, isMac, kebabToCamel, noop, writeZipFile } from './helpers.js';
+export { ensureIsValidSemverTag, createReleaseNotesFromCurrentTag, createGithubRelease, createLabelInJiraIssues } from './release.js';
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,20 @@
-/* eslint-disable no-console, no-process-env, require-atomic-updates, no-await-in-loop */
 import del from 'del';
+import path from 'path';
 import gulp from 'gulp';
-import { EOL } from 'os';
-import axios from 'axios';
-import JSZip from 'jszip';
-import fse from 'fs-extra';
-import semver from 'semver';
 import gulpif from 'gulp-if';
 import esbuild from 'esbuild';
 import eslint from 'gulp-eslint';
-import { promisify } from 'util';
-import ghreleases from 'ghreleases';
 import Graceful from 'node-graceful';
-import axiosRetry from 'axios-retry';
-import { cleanEnv, str } from 'envalid';
-import gitSemverTags from 'git-semver-tags';
-import commitsBetween from 'commits-between';
-import { cliArgs, jest, NodeProcess } from './dev/index.js';
-
-const JIRA_ISSUE_PATTERN = '(EDU|OMA|ELMU)-\\d+';
-const JIRA_BASE_URL = 'https://educandu.atlassian.net';
+import {
+  cliArgs,
+  createGithubRelease,
+  createLabelInJiraIssues,
+  createReleaseNotesFromCurrentTag,
+  ensureIsValidSemverTag,
+  jest,
+  NodeProcess,
+  writeZipFile
+} from './dev/index.js';
 
 let bundler = null;
 let currentApp = null;
@@ -64,6 +59,7 @@ export async function build() {
   if (bundler?.rebuild) {
     await bundler.rebuild();
   } else {
+    // eslint-disable-next-line require-atomic-updates
     bundler = await esbuild.build({
       entryPoints: ['src/index.js'],
       target: ['node14'],
@@ -78,19 +74,21 @@ export async function build() {
 }
 
 export async function pack() {
-  const archive = new JSZip();
-  archive.file('index.js', await fse.readFile('./dist/index.js'));
-  const buffer = await archive.generateAsync({ type: 'nodebuffer' });
-  await fse.ensureDir('./pack');
-  await fse.writeFile('./pack/lambda.zip', buffer);
+  await writeZipFile('./pack/lambda.zip', {
+    'index.js': './dist/index.js'
+  });
 }
 
 export async function startServer() {
   currentApp = new NodeProcess({
-    script: 'test-app/index.js',
+    script: 'src/dev-server.js',
     env: {
+      // eslint-disable-next-line no-process-env
       ...process.env,
-      NODE_ENV: 'development'
+      NODE_ENV: 'development',
+      PORT: (10000).toString(),
+      CDN_BASE_URL: 'http://localhost:9000/dev-educandu-cdn',
+      WEBSITE_BASE_URL: 'http://localhost:3000'
     }
   });
 
@@ -102,66 +100,30 @@ export async function restartServer() {
 }
 
 export function verifySemverTag(done) {
-  if (!semver.valid(cliArgs.tag)) {
-    throw new Error(`Tag ${cliArgs.tag} is not a valid semver string`);
-  }
+  ensureIsValidSemverTag(cliArgs.tag);
   done();
 }
 
 export async function release() {
-  const { GITHUB_REPOSITORY, GITHUB_SERVER_URL, GITHUB_ACTOR, GITHUB_TOKEN } = cleanEnv(process.env, {
-    GITHUB_REPOSITORY: str(),
-    GITHUB_SERVER_URL: str(),
-    GITHUB_ACTOR: str(),
-    GITHUB_TOKEN: str()
+  const { currentTag, releaseNotes, jiraIssueKeys } = await createReleaseNotesFromCurrentTag({
+    jiraBaseUrl: cliArgs.jiraBaseUrl,
+    jiraProjectKeys: cliArgs.jiraProjectKeys.split(',')
   });
 
-  const [githubOrgaName, githubRepoName] = GITHUB_REPOSITORY.split('/');
-  const githubBaseUrl = `${GITHUB_SERVER_URL}/${githubOrgaName}/${githubRepoName}`;
-
-  const [currentTag, previousTag] = await promisify(gitSemverTags)();
-
-  const commits = previousTag
-    ? await commitsBetween({ from: previousTag, to: currentTag })
-    : await commitsBetween();
-
-  const commitListMarkdown = commits.map(commit => {
-    const message = commit.subject
-      .replace(/#\d+/g, num => `[\\${num}](${githubBaseUrl}/pull/${num.replace(/^#/, '')})`)
-      .replace(new RegExp(JIRA_ISSUE_PATTERN, 'g'), num => `[${num}](${JIRA_BASE_URL}/browse/${num})`);
-    const sha = `[${commit.commit.short}](${githubBaseUrl}/tree/${commit.commit.short})`;
-    return `* ${message} (${sha})${EOL}`;
-  }).join('');
-
-  const releaseNotes = previousTag
-    ? `${commitListMarkdown}${EOL}[View all changes](${githubBaseUrl}/compare/${previousTag}...${currentTag})${EOL}`
-    : commitListMarkdown;
-
-  console.log(`Creating Github release ${currentTag}`);
-  await promisify(ghreleases.create)({ user: GITHUB_ACTOR, token: GITHUB_TOKEN }, githubOrgaName, githubRepoName, {
-    // eslint-disable-next-line camelcase
-    tag_name: currentTag,
-    name: currentTag,
-    body: releaseNotes,
-    prerelease: !!semver.prerelease(currentTag)
+  await createGithubRelease({
+    githubToken: cliArgs.githubToken,
+    currentTag,
+    releaseNotes,
+    files: [path.resolve('./pack/lambda.zip')]
   });
 
-  const client = axios.create({ baseURL: JIRA_BASE_URL });
-  axiosRetry(client, { retries: 3 });
-
-  const issueKeys = [...new Set(releaseNotes.match(new RegExp(JIRA_ISSUE_PATTERN, 'g')) || [])].sort();
-  for (const issueKey of issueKeys) {
-    console.log(`Setting label ${currentTag} on JIRA issue ${issueKey}`);
-    try {
-      await client.put(
-        `/rest/api/3/issue/${encodeURIComponent(issueKey)}`,
-        { update: { labels: [{ add: currentTag }] } },
-        { responseType: 'json', auth: { username: cliArgs.jiraUser, password: cliArgs.jiraApiKey } }
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  await createLabelInJiraIssues({
+    jiraBaseUrl: cliArgs.jiraBaseUrl,
+    jiraUser: cliArgs.jiraUser,
+    jiraApiKey: cliArgs.jiraApiKey,
+    jiraIssueKeys,
+    label: currentTag
+  });
 }
 
 export const serve = gulp.series(build, startServer);
