b8b4c6ecf6ca19f7cad016c9ebe4efb6e9d38b90
Adriana Luchian <adriana.dumitras@gmail.de>
2023-01-04 09:20:20 +0100

    EDU-1020 Send rooms auth secret tp access auth endpoint

--- src/lambda/config.js
+++ src/lambda/config.js
@@ -8,7 +8,13 @@ if (!SESSION_COOKIE_NAME) {
   throw new Error('Environment variable SESSION_COOKIE_NAME is not defined');
 }
 
+export const X_ROOMS_AUTH_SECRET = process.env.X_ROOMS_AUTH_SECRET;
+if (!X_ROOMS_AUTH_SECRET) {
+  throw new Error('Environment variable X_ROOMS_AUTH_SECRET is not defined');
+}
+
 // The CDN is supposed to have the same protocol as the website!
 export const CLOUDFRONT_PROTO = new URL(WEBSITE_BASE_URL).protocol;
 
 export const DISABLE_LOGGING = process.env.DISABLE_LOGGING === true.toString();
+
--- src/test-setup.js
+++ src/test-setup.js
@@ -1,3 +1,4 @@
 process.env.DISABLE_LOGGING = true.toString();
 process.env.SESSION_COOKIE_NAME = 'SESSION_ID';
 process.env.WEBSITE_BASE_URL = 'http://localhost:3000';
+process.env.X_ROOMS_AUTH_SECRET = '5BEDA09B74F1432799854164D8361D23';
