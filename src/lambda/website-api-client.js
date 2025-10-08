043481e270c22c87b8df1f5de021eb7e3df08215
Andreas Helmberger <public@ahelmberger.de>
2025-10-09 00:04:22 +0200

    EDU-1768 Compile to node v20 compatible code and remove phin

--- src/lambda/website-api-client.js
+++ src/lambda/website-api-client.js
@@ -1,12 +1,5 @@
-import phin from 'phin';
 import { SESSION_COOKIE_NAME, X_ROOMS_AUTH_SECRET } from './config.js';
 import { getDocumentInputAccessAuthorizationEndpointUrl, getMediaTrashAccessAuthorizationEndpointUrl, getRoomAccessAuthorizationEndpointUrl } from './urls.js';
-
-// Until Lambda@Edge supports node v18.x we have to polyfill native global `fetch`:
-globalThis.fetch ??= async function fetch(url, options) {
-  const response = await phin({ url, ...options });
-  return { status: response.statusCode };
-};
 
 export default class WebsiteApiClient {
   callRoomAccessAuthEndpoint(roomId, sessionCookie) {
